624cc05cac8ed0b58ae9b6df473dfe8768c9688b
Rane Gillian Villanueva <rgvillanueva28@gmail.com>
2022-11-09 19:44:29 +0800

    Done Tasks 1-3

--- src/App.js
+++ src/App.js
@@ -1,23 +1,55 @@
-import logo from './logo.svg';
-import './App.css';
+import { providers } from "ethers";
+import { useState } from "react";
 
 function App() {
+  const [accountAddress, setAccountAddress] = useState("Not connected");
+  const [networkConnected, setNetworkConnected] = useState("Not connected");
+  const [provider, setProvider] = useState(undefined);
+
+  async function connectWallet() {
+    try {
+      const currentProvider = new providers.Web3Provider(
+        window.ethereum,
+        "any"
+      );
+
+      let accounts = await currentProvider.send("eth_requestAccounts", []);
+      const networkDetails = await currentProvider.getNetwork();
+      setNetworkConnected(networkDetails.name.toUpperCase());
+      currentProvider.on("network", (newNetwork) => {
+        setNetworkConnected(newNetwork.name.toUpperCase());
+      });
+      setAccountAddress(accounts[0]);
+      setProvider(currentProvider);
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div
+      style={{
+        display: "flex",
+        width: "100vw",
+        height: "100vh",
+        flexDirection: "column",
+        justifyContent: "center",
+        alignItems: "center",
+      }}
+    >
+      <button
+        disabled={provider}
+        style={{
+          marginLeft: "auto",
+          marginRight: "auto",
+          padding: "10px",
+        }}
+        onClick={connectWallet}
+      >
+        {provider ? "Wallet Connected" : "Connect Wallet"}
+      </button>
+      {provider && <p>Wallet Address: {accountAddress}</p>}
+      {provider && <p>Current Network: {networkConnected}</p>}
     </div>
   );
 }
